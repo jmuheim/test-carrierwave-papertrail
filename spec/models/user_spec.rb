081c9f805787e1bc717265a98c4fdd6a86d83edc
Joshua Muheim <josh@muheimwebdesign.ch>
2015-04-14 11:17:58 +0200

    Refactor specs to make more understandable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,32 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'versioning', versioning: true do
-    context 'avatar' do
-      it 'reloads the original file name for "avatar" on reload (after reify)' do
-        user = User.create! name:   'donald',
-                            avatar: File.open(File.expand_path('spec/support/dummy_files/image.jpg'))
+    it 'reloads the original file name for "avatar" on reload (after reify)' do
+      user = create_and_update_user_with(:avatar)
 
-        user.update_attributes! avatar: File.open(File.expand_path('spec/support/dummy_files/other_image.jpg'))
-        user.versions.last.reify.save
-        user.reload
-
-        expect(user.avatar.file.filename).to match /^image\.jpg$/
-      end
+      expect(user.avatar.file.filename).to match /^image\.jpg$/
     end
 
-    context 'keeping files avatar' do
-      it 'reloads the original file name for "keeping_files_avatar" on reload (after reify)' do
-        user = User.create! name:                 'donald',
-                            keeping_files_avatar: File.open(File.expand_path('spec/support/dummy_files/image.jpg'))
+    it 'reloads the original file name for "keeping_files_avatar" on reload (after reify)' do
+      user = create_and_update_user_with(:keeping_files_avatar)
 
-        user.update_attributes! keeping_files_avatar: File.open(File.expand_path('spec/support/dummy_files/other_image.jpg'))
-        user.versions.last.reify.save
-        user.reload
+      expect(user.keeping_files_avatar.file.filename).to match /^image\.jpg$/
+    end
 
-        expect(user.keeping_files_avatar.file.filename).to match /^image\.jpg$/
-      end
+    # Helper
+    def create_and_update_user_with(field)
+      # Create a first version with image.jpg
+      user = User.create! field => File.open(File.expand_path('spec/support/dummy_files/image.jpg'))
+
+      # Create a 2nd version with other_image.jpg
+      user.update_attribute field, File.open(File.expand_path('spec/support/dummy_files/other_image.jpg'))
+      user.save!
+
+      # Reify version with image.jpg and reload
+      user.versions.last.reify.save!
+      user.reload
+
+      user
     end
   end
 end
