d42ddfec1a7e92b46c8cb90423b8232940c6b33a
Joshua Muheim <josh@muheimwebdesign.ch>
2015-04-14 10:21:04 +0200

    Specs are working without monkey patching

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_paper_trail only: [:name, :email, :avatar]
+  mount_uploader :avatar, AvatarUploader
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'versioning', versioning: true do
+    before do
+      @user = User.create! name:   'donald',
+                           avatar: File.open(File.expand_path('spec/support/dummy_files/image.jpg'))
+    end
+
+    it 'restores the original file name on reify' do
+      # Check for original values
+      expect(@user.name).to eq 'donald'
+      expect(@user.avatar.file.filename).to eq 'image.jpg'
+
+      @user.update_attributes! name:   'newname',
+                               avatar: File.open(File.expand_path('spec/support/dummy_files/other_image.jpg'))
+
+      # Make sure new values are applied
+      expect(@user.name).to eq 'newname'
+      expect(@user.avatar.file.filename).to match 'other_image.jpg'
+
+      # Reify original version and reload
+      @user.versions.last.reify.save
+      @user.reload
+
+      # Check for original values (again)
+      expect(@user.name).to eq 'donald' # Passes
+      expect(@user.avatar.file.filename).to eq 'image.jpg'
+    end
+    #
+    # it 'reloads the original file name on reload after reify' do
+    #   @user = create :@user, :donald, :with_avatar
+    #
+    #   @user.update_attributes! avatar: dummy_file('other_image.jpg')
+    #   @user.versions.last.reify.save
+    #   @user.reload
+    #
+    #   expect(@user.avatar.file.filename).to match /-image\.jpg$/ # Fails: expected "1428993650-43424-5649-other_image.jpg" to match /-image\.jpg$/
+    # end
+  end
 end
